68d543b6c0d72c56af96e84007a69362e9464dfc
rohansaini55632-create <rohansaini55632@gmail.com>
2026-03-31 07:31:56 +0000

    save

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -7,14 +7,29 @@ import Demo2 from './component/Demo2'
 
 function App() {
   
-
+ function  callfun()
+    {
+        alert("Function call");
+    }
+  function  callfun1()
+    {
+        alert("You are click on apple button");
+    }
+  function  callfun2()
+    {
+        alert("You are click on banana button");
+    }
   return (
     <>
     <div>
-      <h1>This is an example of component</h1>
-      <Example></Example>
-      <Demo></Demo>
-      <Demo2></Demo2>
+      {/* <h1>This is an example of component</h1> */}
+      <h1>Welcome in new project</h1>
+      {/* <Example></Example> */}
+      {/* <Demo></Demo> */}
+      {/* <Demo2></Demo2> */}
+      <button onClick={callfun}>OK</button>
+      <button onClick={callfun1}>Apple</button>
+      <button onClick={callfun2}>Banana</button>
     </div>
     </>
   )
